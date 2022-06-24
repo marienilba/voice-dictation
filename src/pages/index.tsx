8f57accbe719425dd1d8adba606e204e8fcee7bd
maelnilba <mael.rioux1997@outlook.fr>
2022-06-24 10:31:32 +0200

    edit title, add github link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import Head from "next/head";
 import { Editor } from "../components/Editor";
 
 const Index: NextPage = () => {
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen h-screen flex flex-col">
       <Head>
-        <title>Next - Speech Recognition</title>
+        <title>Next - Voice Dictation</title>
         <meta
           name="description"
           content="Next App Rich Editor Text + Speech Recognition"
@@ -14,11 +15,26 @@ const Index: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="w-full h-full flex flex-col justify-center items-center">
+      <div className="flex grow flex-col justify-center items-center">
         <Editor />
       </div>
 
-      <footer></footer>
+      <footer className="mb-4">
+        <div className="flex justify-center space-x-2 items-center mb-2 text-lg font-bold hover:cursor-pointer hover:underline ">
+          <Image
+            src="/images/logos/GitHub.png"
+            width="24px"
+            height="24px"
+            // layout="responsive"
+          />
+          <a
+            target="_blank"
+            href={"https://github.com/maelnilba/next-voice-dictation"}
+          >
+            Github
+          </a>
+        </div>
+      </footer>
     </div>
   );
 };
