a7bd7a570d67728e0d7beb8e3c95f623703eda42
maelnilba <mael.rioux1997@outlook.fr>
2022-06-24 10:39:28 +0200

    add 404 handler

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import "regenerator-runtime/runtime";
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppContext, AppProps } from "next/app";
+import App from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await App.getInitialProps(appContext);
+
+  if (appContext.ctx.res?.statusCode === 404) {
+    appContext.ctx.res.writeHead(302, { Location: "/" });
+    appContext.ctx.res.end();
+    return;
+  }
+
+  return { ...appProps };
+};
 
 export default MyApp;
